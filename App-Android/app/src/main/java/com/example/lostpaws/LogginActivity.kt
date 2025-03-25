4c5183a33a04b0b3e97b0f2172380424e51575fc
RMTorrabadella04 <raul.martin.torrabadella@gmail.com>
2025-03-25 22:17:34 +0100

    Se ha creado todo lo referente a lo visual del loggin y registro

---
+++ App-Android/app/src/main/java/com/example/lostpaws/LogginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.lostpaws
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LogginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_loggin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnLoggin = findViewById<Button>(R.id.loggin)
+        btnLoggin.setOnClickListener {
+            Toast.makeText(this, "La función de logearse estara disponible proximamente", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
